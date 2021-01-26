6ef990bb88a5d9fbd09a06b5d70bdc24f52a9bfb
Ksarqu <ksarqu@gmail.com>
2021-01-26 21:59:52 +0100

    1.0

---
+++ rambreaker.cpp
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << endl;
+    cout << endl;
+    cout << endl;
+    cout << "Co chcesz zrobić" << endl;
+    cout << endl;
+    cout << "1 - Wypelnij ram i swap" << endl;
+    cout << endl;
+    cout << "2 - Wyjdz";
+    cout << endl;
+    int wybor;
+    cin >> wybor;
+    if (wybor == 1) {
+        while (1) {
+            malloc((size_t)2048);
+        }
+    }
+    
+    
+}
